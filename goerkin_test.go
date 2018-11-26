b94053403ca419f05b1d6efbf7a06d1a2092d7d8
Rasheed Abdul-Aziz <rabdulaziz@pivotal.io>
2018-11-25 16:12:53 -0800

    first attempt at matching groups

--- goerkin_test.go
+++ goerkin_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/bunniesandbeatings/goerkin"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"strconv"
 )
 
 var _ = Describe("Inline Definitions", func() {
@@ -73,6 +74,30 @@ var _ = Describe("Scenario first", func() {
 	steps.Define(func(define Definitions) {
 		define.Then("^it works$", func() {
 			Succeed()
+		})
+	})
+
+})
+
+var _ = Describe("Groups as params", func() {
+	var total int
+	steps := NewSteps()
+
+	Scenario("Use RE groups as params", func() {
+		steps.Given("4 and 6")
+		steps.Then("the answer is 10")
+	})
+
+	steps.Define(func(define Definitions) {
+		// TODO, reflect and report so the func can expect ints
+		define.Given(`^(\d+) and (\d+)$`, func(first, second string) {
+			firstInt, _ := strconv.Atoi(first)
+			secondInt, _ := strconv.Atoi(second)
+			total = firstInt + secondInt
+		})
+		define.Then(`^the answer is (\d+)$`, func(answer string) {
+			answerInt, _ := strconv.Atoi(answer)
+			Expect(total).To(Equal(answerInt))
 		})
 	})
 
--- steps.go
+++ steps.go
@@ -3,6 +3,7 @@ package goerkin
 import (
 	"fmt"
 	"github.com/onsi/ginkgo"
+	"reflect"
 )
 
 type Steps struct {
@@ -31,25 +32,33 @@ func (s *Steps) Define(body defineBodyFn) {
 	body(s.definitions)
 }
 
+type matchT struct {
+	body interface{}
+	params []string
+}
+
 func (s *Steps) run(method, text string, override []bodyFn) {
 	if len(override) > 0 {
 		ginkgo.By(text, override[0])
 		return
 	}
 
-	var match bodyFn
+	match := matchT{}
 
 	for re, body := range s.definitions {
-		if re.MatchString(text) {
-			if match != nil {
-				ginkgo.Fail(fmt.Sprintf("Too many matches for `%s`", text))
-				return
-			}
-			match = body
+		stringMatches := re.FindStringSubmatch(text)
+		if stringMatches == nil { continue }
+
+		if match.body != nil {
+			ginkgo.Fail(fmt.Sprintf("Too many matches for `%s`", text))
+			return
 		}
+
+		match.body = body
+		match.params = stringMatches[1:]
 	}
 
-	if match == nil {
+	if match.body == nil {
 		templateBacktick := fmt.Sprintf("define.%s(`^%s$`, func() {})", method, text)
 		templateDouble := fmt.Sprintf("define.%s(\"^%s$\", func() {})", method, text)
 		// FIXME: matches fail here, they should show the definition that failed
@@ -57,7 +66,24 @@ func (s *Steps) run(method, text string, override []bodyFn) {
 		return
 	}
 
-	ginkgo.By(text, match)
+	ginkgo.By(text, func() {
+		switch match.body.(type) {
+		case func():
+			match.body.(func())()
+		default:
+			matchValue := reflect.ValueOf(match.body)
+
+			in := make([]reflect.Value, len(match.params))
+
+			for paramIndex := range in {
+				in[paramIndex] = reflect.ValueOf(match.params[paramIndex])
+			}
+
+			matchValue.Call(in)
+
+			//ginkgo.Fail(fmt.Sprintf("Could not match function call for \"%s\"\nlooking for:%v", text, reflect.TypeOf(match)))
+		}
+	})
 }
 
 func (s *Steps) Given(text string, body ...bodyFn) { s.run("Given", text, body) }
@@ -65,4 +91,4 @@ func (s *Steps) When(text string, body ...bodyFn)  { s.run("When", text, body) }
 func (s *Steps) Then(text string, body ...bodyFn)  { s.run("Then", text, body) }
 func (s *Steps) And(text string, body ...bodyFn)   { s.run("And", text, body) }
 
-func (s *Steps) Run(text string)   { s.run("Step", text, nil) }
+func (s *Steps) Run(text string) { s.run("Step", text, nil) }
